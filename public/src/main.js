0d8bad654038b7e688eb44a4c232680486fe422d
BlackxLegend <127228556+BlackxLegend@users.noreply.github.com>
2024-12-14 01:40:37 +0600

    Delete public directory

--- public/src/main.js
+++
@@ -1,9 +0,0 @@
-import { setupCountdown } from './js/countdown';
-import { setupAnimations } from './js/animations';
-import { setupSubscribe } from './js/subscribe';
-
-document.addEventListener('DOMContentLoaded', () => {
-  setupCountdown();
-  setupAnimations();
-  setupSubscribe();
-});
